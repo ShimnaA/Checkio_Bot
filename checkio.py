f62447868597aae9d78077071c66631d5732664c
unknown <shimnaa.arangil@gmail.com>
2020-06-05 13:59:02 +0530

    Completed Credentials and Base Class

---
+++ checkio.py
@@ -0,0 +1,19 @@
+import json
+
+def read_credentials():
+    secrets = "secrets.json"
+    with open(secrets) as f:
+        keys = json.loads(f.read())
+        return keys
+
+class CheckIOSolver:
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+    
+
+if __name__ == '__main__':
+    credentials = read_credentials()
+    bot = CheckIOSolver(credentials['username'], credentials['password'])
+    print(bot.login)
+    print(bot.password)
