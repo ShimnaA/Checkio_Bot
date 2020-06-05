2a6d9411bcd42219815a8c6489aaa0dc3f8b0e7f
unknown <shimnaa.arangil@gmail.com>
2020-06-05 17:06:21 +0530

    Login to checkio completed

--- checkio.py
+++ checkio.py
@@ -1,4 +1,8 @@
 import json
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+import time
 
 def read_credentials():
     secrets = "secrets.json"
@@ -10,10 +14,47 @@ class CheckIOSolver:
     def __init__(self, login, password):
         self.login = login
         self.password = password
-    
+        self.google = "https://www.google.com/"
+        self.base_url = "https://checkio.org/"
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    def login_to_checkio(self):
+        self.driver.get(self.base_url)
+        self.get_on_python_checkio()
+        self.put_credentials_to_form()
+
+    def put_credentials_to_form(self):
+        try:
+            # Enter user credentials and Submit the form
+            self.driver.find_element_by_id("id_username").send_keys(self.login)
+            time.sleep(1)
+            password_field = self.driver.find_element_by_id("id_password")
+            password_field.send_keys(self.password)
+            password_field.submit()
+            time.sleep(1)
+
+        except NoSuchElementException:
+            print("Exception NoSuchElementException")
+
+    def get_on_python_checkio(self):
+        try:
+            self.driver.find_element_by_link_text('Python').click()
+            time.sleep(2)
+        except NoSuchElementException:
+            print("incorrect Page")
+
+
+
+    def main_logic(self):
+        self.login_to_checkio()
+
+
+
+        time.sleep(3)
+        self.driver.quit()
+
 
 if __name__ == '__main__':
     credentials = read_credentials()
     bot = CheckIOSolver(credentials['username'], credentials['password'])
-    print(bot.login)
-    print(bot.password)
+    bot.main_logic()
