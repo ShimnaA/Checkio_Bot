7938e39ae3b4c61eb47f4e22c3022eaff396e788
unknown <shimnaa.arangil@gmail.com>
2020-06-09 17:21:49 +0530

    Completed Pasting the Copied Solution in CheckIO and chek

--- checkio.py
+++ checkio.py
@@ -2,6 +2,7 @@ import json
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 import time
 
 
@@ -38,9 +39,12 @@ class CheckIOSolver:
         self.task_ToSolve_List = []
         self.chekio_mainpage_url = ""
         self.current_solvingTask_url = ""
+        self.current_google_result_link =[]
+        self.curr_google_solution_code = []
 
     def login_to_checkio(self):
         self.driver.get(self.base_url)
+        self.driver.maximize_window()
         self.get_on_python_checkio()
         self.put_credentials_to_form()
 
@@ -112,6 +116,7 @@ class CheckIOSolver:
         time.sleep(2)
         self.current_solvingTask_url = self.driver.current_url
 
+
     def get_google_search_result(self,task):
         #Navigate to Google.com and enter the Search text "Python checkIO {TASK NAME}"
         self.driver.get(self.google)
@@ -120,15 +125,71 @@ class CheckIOSolver:
         searchbox.send_keys(self.SEARCH_TEXT + task.title)
         time.sleep(1)
         searchbox.submit()
-
-        #driver.find_elements_by_xpath("//input[@value='Google Search']")[1].click()
         time.sleep(2)
+
+        google_results = self.driver.find_elements_by_xpath("//div[@class='r']/a")
+        result_count = 0
+        self.current_google_result_link = []
+        print("No of google results " + str(len(google_results)))
+        for results in google_results:
+            href = results.get_attribute("href")
+            if 'publications' in href:
+                self.current_google_result_link.append(href)
+                result_count += 1
+            if result_count > 5: break
+        time.sleep(3)
+        print(self.current_google_result_link)
+
+    def get_solution_code(self, google_result_link):
+        self.driver.get(google_result_link)
+        time.sleep(2)
+        publications_code = self.driver.find_element_by_xpath("//div[@class='publications__info__code']")
+        solution_code = publications_code.find_elements_by_xpath("//span[@style='padding-right: 0.1px;']")
+        self.curr_google_solution_code = []
+        for data in solution_code:
+            code_words = data.find_elements_by_css_selector('span')
+            code_line = ""
+            for word in code_words:
+                code_line += word.text
+            print(code_line)
+            self.curr_google_solution_code.append(code_line)
+
+    def check_current_solution(self):
+        self.driver.get(self.current_solvingTask_url)
+        solution_textarea_element = self.driver.find_element_by_xpath("//textarea")
+        solution_textarea_element.send_keys(Keys.CONTROL + "a")
+        solution_textarea_element.send_keys(Keys.DELETE)
+        time.sleep(1)
+
+        for i in self.curr_google_solution_code:
+            solution_textarea_element.send_keys(i)
+            solution_textarea_element.send_keys('\n')
+            solution_textarea_element.send_keys(Keys.HOME)
+        time.sleep(10)
+
+        self.driver.find_element_by_id('check-code-btnEl').click()
+        time.sleep(10)
+        success_element = self.driver.find_elements_by_xpath("//div[@class='animation-success']")
+        if len(success_element)>0:
+            print("SUCCESS Completed Task " + self.current_solvingTask_url)
+            return True
+        else:
+            print("Failed Task " + self.current_solvingTask_url)
+            return False
+
+
+    def solve_current_task(self,task):
+        for result_link in self.current_google_result_link:
+            self.get_solution_code(result_link)
+            if self.check_current_solution(): break
 
 
     def solve_all_tasks_in_station(self):
         for task in self.task_ToSolve_List:
-            self.navigate_to_taskSolvepage(task)
             self.get_google_search_result(task)
+            self.navigate_to_taskSolvepage(task)
+            self.solve_current_task(task)
+
 
 
     def get_all_missions(self):
